8f414b3bce1c2818f1fe3a3871769e95e6b2e7cc
esperblok2 <blokesper5@gmail.com>
2026-07-21 12:56:13 +0200

    Volledige Android app: themes, HID, updates, notes, 8 tabs

--- android-app/android/app/src/main/java/com/esperblok/btscanner/MainActivity.kt
+++ android-app/android/app/src/main/java/com/esperblok/btscanner/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.esperblok.btscanner
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.esperblok.btscanner.fragments.*
@@ -10,13 +13,19 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var bluetoothHelper: BluetoothHelper
     lateinit var securityManager: SecurityManager
+    lateinit var hidHelper: HidHelper
+    lateinit var deviceNotesManager: DeviceNotesManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ThemeManager.loadTheme(this)
         setContentView(R.layout.activity_main)
+        ThemeManager.applyTheme(this)
 
         bluetoothHelper = BluetoothHelper(this)
         securityManager = SecurityManager(this)
+        hidHelper = HidHelper(this)
+        deviceNotesManager = DeviceNotesManager(this)
 
         bluetoothHelper.registerReceiver()
 
@@ -31,12 +40,45 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_scan -> { loadFragment(ScanFragment()); true }
                 R.id.nav_paired -> { loadFragment(PairedFragment()); true }
                 R.id.nav_control -> { loadFragment(ControlFragment()); true }
+                R.id.nav_hid -> { loadFragment(HidFragment()); true }
                 R.id.nav_battery -> { loadFragment(BatteryFragment()); true }
                 R.id.nav_local -> { loadFragment(LocalFragment()); true }
                 R.id.nav_security -> { loadFragment(SecurityFragment()); true }
+                R.id.nav_update -> { loadFragment(UpdateFragment()); true }
                 else -> false
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menu.add(0, 1001, 0, "Thema Wisselen")
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == 1001) {
+            showThemePicker()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showThemePicker() {
+        val names = ThemeManager.getThemeNames().toTypedArray()
+        val current = ThemeManager.getCurrentTheme().name
+        val checkedIndex = names.indexOf(current)
+
+        AlertDialog.Builder(this)
+            .setTitle("Kies een thema")
+            .setSingleChoiceItems(names, checkedIndex) { dialog, which ->
+                val selected = names[which]
+                ThemeManager.saveTheme(this, selected)
+                ThemeManager.setTheme(selected)
+                dialog.dismiss()
+                recreate()
+            }
+            .setNegativeButton("Annuleren", null)
+            .show()
     }
 
     private fun loadFragment(fragment: Fragment) {
@@ -49,5 +91,6 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         bluetoothHelper.stopScan()
         bluetoothHelper.unregisterReceiver()
+        hidHelper.disconnect()
     }
 }
